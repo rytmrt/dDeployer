b9a76a5e3c911a4479ccab3e529b6a8113384434
Ryota Morita <ryota.morita.3.8@gmail.com>
2015-02-21 18:56:49 +0900

    読みにくかったので、リファクタリング

--- ddeployer.rb
+++ ddeployer.rb
@@ -1,62 +1,95 @@
 require 'digest/md5'
 
-script_path = File.dirname(__FILE__)
-script_dir = File.expand_path(script_path)
+SCRIPT_PATH = File.dirname(__FILE__)
+SCRIPT_DIR = File.expand_path(SCRIPT_PATH)
+
+BRANCH = 'master'
+`git checkout #{BRANCH}`
 
 REPOSITORY_PATH = Dir.pwd
 REPOSITORY_NAME = File.basename(REPOSITORY_PATH)
 REPOSITORY_HASH = Digest::MD5.new.update(REPOSITORY_PATH).to_s
-branch_name = 'master'
+REPOSITORY_HEAD = `git rev-parse HEAD`.chomp
 
-TMP_FILE = "#{script_dir}/tmp/#{REPOSITORY_NAME}-#{branch_name}-#{REPOSITORY_HASH}"
+TMP_FILE = "#{SCRIPT_DIR}/tmp/#{REPOSITORY_NAME}-#{BRANCH}-#{REPOSITORY_HASH}"
 
-forwardlist = []
-removelist = []
+def create_list_object()
+  return {:forward => [],:remove => []}
+end
 
-if ! File.exist?(TMP_FILE) then
-  puts 'not exist log file.'
-  cmd = "git ls-files"
-  responce_cmd = `#{cmd}`
+def get_diff_file_list(commit1, commit2)
+  list = create_list_object()
+  responce_cmd = `git diff --name-status #{commit1} #{commit2}`
   responce_cmd.each_line do |line|
-    forwardlist << line.chomp
+    strs = line.chomp.split("\t")
+    case strs[0]
+    when "A","M"
+      list[:forward] << strs[1]
+    when "D"
+      list[:remove] << strs[1]
+    end
   end
-  File.open(TMP_FILE, 'a') do |file|
-    str = "#{Time.now.to_s}\t#{`git rev-parse HEAD`}"
-    file.puts(str)
+  return list
+end
+
+def get_all_file_list()
+  list = create_list_object()
+  responce_cmd = `git ls-files`
+  responce_cmd.each_line do |line|
+    list[:forward] << line.chomp
   end
-else
-  puts 'exist log file.'
-  prev_hash = ''
-  head_hash = `git rev-parse HEAD`.chomp
-  open("|tail -n 1 < #{TMP_FILE}") do |fi|
+  return list
+end
+
+def get_last_hash(file)
+  hash = nil
+  open("|tail -n 1 < #{file}") do |fi|
     line = fi.gets(nil)
     strs = line.chomp.split("\t")
-    prev_hash = strs[1]
+    hash = strs[1]
   end
-  if head_hash != prev_hash
+  return hash
+end
+
+def deploy(deploy_info)
+  do_deploy = false
+  if deploy_info[:forward].count > 0 then
+    puts "Forward list ======"
+    p deploy_info[:forward]
+    do_deploy = true
+  end
+  if deploy_info[:remove].count > 0 then
+    puts "Remove list ======="
+    p deploy_info[:remove]
+    do_deploy = true
+  end
+  return do_deploy
+end
+
+def write_log(file, str)
+  File.open(file, 'a') do |file|
+    file.puts(str)
+  end
+end
+
+
+# MAIN -------------------------------------------------------------------------
+file_list = nil
+if ! File.exist?(TMP_FILE) then
+  puts 'not exist log file.'
+  file_list = get_all_file_list()
+else
+  puts 'exist log file.'
+  last_hash = get_last_hash(TMP_FILE)
+  if REPOSITORY_HEAD != last_hash
     puts "Different hash"
-    cmd = "git diff --name-status #{prev_hash} #{head_hash}"
-    responce_cmd = `#{cmd}`
-    responce_cmd.each_line do |line|
-      strs = line.chomp.split("\t")
-      case strs[0]
-      when "A","M"
-        forwardlist << strs[1]
-      when "D"
-        removelist << strs[1]
-      end
-    end
-    File.open(TMP_FILE, 'a') do |file|
-      str = "#{Time.now.to_s}\t#{head_hash}"
-      file.puts(str)
-    end
+    file_list = get_diff_file_list(last_hash, REPOSITORY_HEAD)
   else
     puts "Same hash."
     exit
   end
 end
 
-puts "Forward list ======"
-p forwardlist
-puts "Remove list ======="
-p removelist
+if deploy(file_list)
+  write_log(TMP_FILE, "#{Time.now.to_s}\t#{REPOSITORY_HEAD}")
+end
