fff9b5cf6ec603a7ad473217a9c6c0473e33d184
Ryota Morita <ryota.morita.3.8@gmail.com>
2015-02-21 16:14:56 +0900

    変数名をリファクタリング

--- ddeployer.rb
+++ ddeployer.rb
@@ -3,22 +3,24 @@ require 'digest/md5'
 script_path = File.dirname(__FILE__)
 script_dir = File.expand_path(script_path)
 
-repository_name = File.basename(Dir.pwd)
-repository_hash = Digest::MD5.new.update(Dir.pwd).to_s
+REPOSITORY_PATH = Dir.pwd
+REPOSITORY_NAME = File.basename(REPOSITORY_PATH)
+REPOSITORY_HASH = Digest::MD5.new.update(REPOSITORY_PATH).to_s
 branch_name = 'master'
-prev_file = "#{script_dir}/tmp/#{repository_name}-#{branch_name}-#{repository_hash}"
+
+TMP_FILE = "#{script_dir}/tmp/#{REPOSITORY_NAME}-#{branch_name}-#{REPOSITORY_HASH}"
 
 forwardlist = []
 removelist = []
 
-if ! File.exist?(prev_file) then
+if ! File.exist?(TMP_FILE) then
   puts 'not exist log file.'
   cmd = "git ls-files"
   responce_cmd = `#{cmd}`
   responce_cmd.each_line do |line|
     forwardlist << line.chomp
   end
-  File.open(prev_file, 'a') do |file|
+  File.open(TMP_FILE, 'a') do |file|
     str = "#{Time.now.to_s}\t#{`git rev-parse HEAD`}"
     file.puts(str)
   end
@@ -26,7 +28,7 @@ else
   puts 'exist log file.'
   prev_hash = ''
   head_hash = `git rev-parse HEAD`.chomp
-  open("|tail -n 1 < #{prev_file}") do |fi|
+  open("|tail -n 1 < #{TMP_FILE}") do |fi|
     line = fi.gets(nil)
     strs = line.chomp.split("\t")
     prev_hash = strs[1]
